52af3d8f0347d65d5b45e57c62005a7f90bbb79c
BurdiApps <aburdick@byupathway.edu>
2025-11-02 21:27:51 -0800

    created letter grade program for exercise.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade %: ");
+        int classGrade = int.Parse(Console.ReadLine());
+
+        if (classGrade >= 97)
+        {
+            Console.WriteLine("You have an +A");
+        }
+        else if (classGrade >= 94)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (classGrade >= 90)
+        {
+            Console.WriteLine("You have an A-");
+        }
+        else if (classGrade >= 87)
+        {
+            Console.WriteLine("You have a B+");
+        }
+        else if (classGrade >= 84)
+        {
+            Console.WriteLine("You have a B");
+        }
+
+        else if (classGrade >= 80)
+        {
+            Console.WriteLine("You have a B-");
+        }
+        else if (classGrade >= 77)
+        {
+            Console.WriteLine("You have a C+");
+        }
+        else if (classGrade >= 74)
+        {
+            Console.WriteLine("You have a C");
+        }
+
+        else if (classGrade >= 70)
+        {
+            Console.WriteLine("You have a C-");
+        }
+
+        else if (classGrade >= 67)
+        {
+            Console.WriteLine("You have a D+");
+        }
+
+        else if (classGrade >= 64)
+        {
+            Console.WriteLine("You have a D");
+        }
+
+        else if (classGrade >= 60)
+        {
+            Console.WriteLine("You have a D-");
+        }
+
+        else
+        {
+            Console.WriteLine("You have an F");
+        }
+
     }
+
 }
