c1bad99ab75d5f45ea82ca1e727b4e4c13b6b600
BurdiApps <aburdick@byupathway.edu>
2025-11-02 19:31:54 -0800

    Finished input Exercise #1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // James Bonds style input practice
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // note to self, add spaces in-between blocks of code for readability
+
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}");
     }
 }
