07660e0167576bb18cc69bdc27b15cf30053b588
BurdiApps <aburdick@byupathway.edu>
2025-11-02 22:22:15 -0800

    Completed exercise #3 with number guessing game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Guess my number game
+        int myNumber = 21;
+        int userGuess;
+        do
+        {
+            Console.WriteLine("What's the magic number? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < myNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (userGuess > myNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (userGuess != myNumber);
     }
 }
