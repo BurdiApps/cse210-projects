cb895281e49bf429becae810d3694074ef973c5a
BurdiApps <aburdick@byupathway.edu>
2025-11-03 18:00:05 -0800

    Finished functions practice with exercise #5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        WelcomeUser();
+        string userName = GetUserName();
+        int favoriteNumber = GetFavoriteNumber();
+        int square = CalculateSquare(favoriteNumber);
+
+        Console.WriteLine($"{userName}, the square of your number is {square}");
+    }
+
+    static void WelcomeUser()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetFavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int CalculateSquare(int number)
+    {
+        return number * number;
     }
 }
